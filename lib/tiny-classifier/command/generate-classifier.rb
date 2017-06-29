4cfb2f5e28218d70e0197a635dd4487da82c19f7
YUKI Hiroshi <shimoda@clear-code.com>
2017-06-29 19:55:18 +0900

    Make tc-generate-classifier testable

--- lib/tiny-classifier/command/generate-classifier.rb
+++ lib/tiny-classifier/command/generate-classifier.rb
@@ -14,32 +14,26 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require "tiny-classifier/command/base"
-require "tiny-classifier/command/classify"
 require "fileutils"
 require "base64"
 
 module TinyClassifier
   module Command
     class GenerateClassifier < Base
-      class << self
-        def run(argv=nil)
-          argv ||= ARGV.dup
-          generator = new
-          generator.parse_command_line_options(argv)
-          generator.run
-        end
-      end
-  
-      def initialize
+      def initialize(argv=[])
         super
+
         @output_dir = Dir.pwd
         option_parser.on("-o PATH", "--output-dir=PATH",
                          "Path to the classifier command to be saved (default=current directory)") do |output_dir|
           @output_dir = output_dir
         end
+
+        parse_command_line_options(argv)
       end
   
       def run
+        super
         File.open(output_file_path, "w") do |file|
           file.puts("#!/usr/bin/env ruby")
           file.puts("require \"base64\"")
@@ -52,28 +46,29 @@ module TinyClassifier
           file.puts("classify.run")
         end
         FileUtils.chmod("a+x", output_file_path)
+        true
+      rescue StandardError => error
+        handle_error(error)
       end
-  
+
+      def classifier_name
+        @classifier_name ||= "tc-classify-#{@categories.basename}"
+      end
+
+      def output_file_path
+        @output_file_path ||= prepare_output_file_path
+      end
+
       private
       def encoded_classifier
         @encoded_classifier ||= prepare_encoded_classifier
       end
   
       def prepare_encoded_classifier
-        classifier = Classifier.new
-        classifier.parse_command_line_options(ARGV.dup)
         FileUtils.mkdir_p(output_file_path.parent)
-  
-        classifier_code = Marshal.dump(classifier.classifier)
+
+        classifier_code = Marshal.dump(classifier)
         Base64.strict_encode64(classifier_code)
-      end
-  
-      def classifier_name
-        @classifier_name ||= "tc-classify-#{@categories.basename}"
-      end
-  
-      def output_file_path
-        @output_file_path ||= prepare_output_file_path
       end
   
       def prepare_output_file_path
